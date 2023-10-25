cae9004a877d9228180078752c09d467b2a2e08b
MFALLAM <mohammed.fathi.allam@gmail.com>
2023-10-25 10:54:14 +0300

    finding TextViews and Editview

--- app/src/main/java/com/example/github_search/MainActivity.java
+++ app/src/main/java/com/example/github_search/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.github_search;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mUrlDisplayTextView;
+    private TextView mSearchResultsTextView;
+    private EditText mSearchBoxEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
